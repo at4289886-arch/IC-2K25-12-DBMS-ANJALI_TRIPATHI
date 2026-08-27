0d90d18950c38f5360ba6e6f5049d213ac1dc1a8
Anjali Tripathi <at4289886@gmail.com>
2026-08-27 14:45:12 +0530

    Question_5.sql
    Create a table 'countries_null' with nullable fields and insert sample country data.

---
+++ Lab _Assignment - 1/Question_5.sql
@@ -0,0 +1,22 @@
+--➡️Question 5):write a sql statement to create table countries a constraint NULL.
+
+
+CREATE TABLE countries_null (
+country_id INT NULL,
+country_name VARCHAR(30) NULL,
+region_id VARCHAR(10) NULL
+);
+INSERT INTO countries_null VALUES(NULL,"INDIA",NULL);
+INSERT INTO countries_null VALUES(NULL,"PAKISTAN",NULL);
+INSERT INTO countries_null VALUES(NULL,"EUROPE",NULL);
+INSERT INTO countries_null VALUES(NULL,"BANGLADESH",NULL);
+INSERT INTO countries_null VALUES(NULL,"FRANCE",NULL);
+INSERT INTO countries_null VALUES(NULL,"CHINA",NULL);
+INSERT INTO countries_null VALUES(NULL,"JAPAN",NULL);
+INSERT INTO countries_null VALUES(NULL,"CANADA",NULL);
+
+SELECT * FROM countries_null;
+
+
+
+
