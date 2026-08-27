7f24c77bccb92fa4996df0f262fff1f16a44904e
Anjali Tripathi <at4289886@gmail.com>
2026-08-27 14:12:51 +0530

    Question_4.sql
    Create a copy of the countries table named dup_countries with structure and data.

---
+++ Lab _Assignment - 1/Question_4.sql
@@ -0,0 +1,35 @@
+--➡️Question 4):write a sql statement to create copy of countries table including structure and data by name dup_countries.
+
+CREATE DATABASE dbms_assignment;
+USE dbms_assignment;
+CREATE TABLE countries(
+country_id INT PRIMARY KEY,
+country_name VARCHAR(30),
+region_id VARCHAR(10)
+);
+INSERT INTO countries VALUES(1,"INDIA","ASIA");
+INSERT INTO countries VALUES(2,"PAKISTAN","ASIA");
+INSERT INTO countries VALUES(3,"UNITED KINGDOM","EUROPE");
+INSERT INTO countries VALUES(4,"BANGLADESH","ASIA");
+INSERT INTO countries VALUES(5,"FRANCE","EUROPE");
+INSERT INTO countries VALUES(6,"CHINA","NA");
+INSERT INTO countries VALUES(7,"JAPAN","ASIA");
+INSERT INTO countries VALUES(8,"CANADA","NA");
+
+SELECT * FROM countries;
+
+CREATE TABLE IF NOT EXISTS countries(
+country_id INT,
+country_name VARCHAR(50),
+region_id VARCHAR(100)
+);
+
+CREATE TABLE dup_countries LIKE countries;
+INSERT INTO dup_countries
+SELECT * FROM countries;
+
+
+
+
+
+
