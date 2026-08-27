13e806262d7b958845b54ac339a165948a5f71c5
Anjali Tripathi <at4289886@gmail.com>
2026-08-27 13:44:10 +0530

    Question_2.sql
    Create a table 'countries' with specified columns.

---
+++ Lab _Assignment - 1/Question_2.sql
@@ -0,0 +1,8 @@
+--➡️ Question 2):write a SQL statement to create a simple table countries including column country_id,country_name,region_id which is already exists.
+
+CREATE TABLE IF NOT EXISTS countries(
+country_id INT,
+country_name VARCHAR(50),
+region_id VARCHAR(100)
+);
+
