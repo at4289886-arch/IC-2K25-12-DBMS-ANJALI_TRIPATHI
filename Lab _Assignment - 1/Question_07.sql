641a0b56dad5ff000399c5c95e951c2cf42ca88a
Anjali Tripathi <at4289886@gmail.com>
2026-08-30 23:19:12 +0530

    Create countries table in and use of CHECK constraient.
    Added SQL statement to create a countries table with constraints.

--- Lab _Assignment - 1/Question_07.sql
+++ Lab _Assignment - 1/Question_07.sql
@@ -1,5 +1,8 @@
 --➡️Question 7):Write a SQL statement to create a table named countries including columns country_id, country_name and region_id and make sure that no countries except Italy, India and China will be entered in the table.
 
+
+
+
 CREATE DATABASE dbms_assignment;
 USE dbms_assignment;
 CREATE TABLE countries(
