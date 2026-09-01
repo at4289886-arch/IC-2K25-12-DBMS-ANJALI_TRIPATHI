865e7627180cd26c449399b8d41d470880ca5d89
Anjali Tripathi <at4289886@gmail.com>
2026-09-01 10:49:37 +0530

    calculate salery in PF.
    Executed multiple SQL queries to retrieve employee data and calculate provident fund (PF) based on salary. Fixed errors related to incorrect column names and table name.

---
+++ Lab _Assignment - 2/Question_04.sql
@@ -0,0 +1,42 @@
+ MySQL  localhost:33060+ ssl  SQL > USE dbms_assignment;
+Default schema set to `dbms_assignment`.
+Fetching global names, object names from `dbms_assignment` for auto-completion... Press ^C to stop.
+  MySQL  localhost:33060+ ssl  dbms_assignment  SQL > SELECT * FROM employees;
++-------------+------------+-----------+------------------+--------------+------------+--------+----------+------------+------------+---------------+
+| employee_id | first_name | last_name | email            | phone_number | hire_date  | job_id | salary   | commission | manager_id | department_id |
++-------------+------------+-----------+------------------+--------------+------------+--------+----------+------------+------------+---------------+
+|           1 | Rahul      | Sharma    | rahul@gmail.com  | 9876543210   | 2023-01-10 |      1 | 30000.00 |    2000.00 |        101 |            10 |
+|           2 | Priya      | Verma     | priya@gmail.com  | 9876543211   | 2023-02-15 |      2 | 35000.00 |    2500.00 |        102 |            20 |
+|           3 | Aman       | Patel     | aman@gmail.com   | 9876543212   | 2023-03-20 |      3 | 28000.00 |    1500.00 |        101 |            10 |
+|           4 | Neha       | Singh     | neha@gmail.com   | 9876543213   | 2023-04-05 |      1 | 40000.00 |    3000.00 |        103 |            30 |
+|           5 | Riya       | Gupta     | riya@gmail.com   | 9876543214   | 2023-05-12 |      2 | 32000.00 |    2200.00 |        102 |            20 |
+|           6 | Karan      | Mehta     | karan@gmail.com  | 9876543215   | 2023-06-18 |      3 | 45000.00 |    3500.00 |        103 |            30 |
+|           7 | Ankit      | Jain      | ankit@gmail.com  | 9876543216   | 2023-07-22 |      1 | 25000.00 |    1000.00 |        101 |            10 |
+|           8 | Pooja      | Yadav     | pooja@gmail.com  | 9876543217   | 2023-08-10 |      2 | 38000.00 |    2800.00 |        102 |            20 |
+|           9 | Vikas      | Shah      | vikas@gmail.com  | 9876543218   | 2023-09-14 |      3 | 50000.00 |    4000.00 |        103 |            30 |
+|          10 | Simran     | Khan      | simran@gmail.com | 9876543219   | 2023-10-25 |      1 | 42000.00 |    3200.00 |        101 |            10 |
++-------------+------------+-----------+------------------+--------------+------------+--------+----------+------------+------------+---------------+
+10 rows in set (0.0484 sec)
+   MySQL  localhost:33060+ ssl  dbms_assignment  SQL > SELECT first_name,last_name,salery,salery*15/100 AS PF from employees;
+ERROR: 1054: Unknown column 'salery' in 'field list'
+ MySQL  localhost:33060+ ssl  dbms_assignment  SQL > SELECT firt_name , last_name , salary ,salary *15/100 AS PF FROM employyes;
+ERROR: 1146: Table 'dbms_assignment.employyes' doesn't exist
+ MySQL  localhost:33060+ ssl  dbms_assignment  SQL > SELECT firt_name , last_name , salary ,salary *15/100 AS PF FROM employees;
+ERROR: 1054: Unknown column 'firt_name' in 'field list'
+ MySQL  localhost:33060+ ssl  dbms_assignment  SQL > SELECT first_name , last_name , salary ,salary *15/100 AS PF FROM employees;
++------------+-----------+----------+-------------+
+| first_name | last_name | salary   | PF          |
++------------+-----------+----------+-------------+
+| Rahul      | Sharma    | 30000.00 | 4500.000000 |
+| Priya      | Verma     | 35000.00 | 5250.000000 |
+| Aman       | Patel     | 28000.00 | 4200.000000 |
+| Neha       | Singh     | 40000.00 | 6000.000000 |
+| Riya       | Gupta     | 32000.00 | 4800.000000 |
+| Karan      | Mehta     | 45000.00 | 6750.000000 |
+| Ankit      | Jain      | 25000.00 | 3750.000000 |
+| Pooja      | Yadav     | 38000.00 | 5700.000000 |
+| Vikas      | Shah      | 50000.00 | 7500.000000 |
+| Simran     | Khan      | 42000.00 | 6300.000000 |
++------------+-----------+----------+-------------+
+10 rows in set (0.0038 sec)
+ MySQL  localhost:33060+ ssl  dbms_assignment  SQL >
