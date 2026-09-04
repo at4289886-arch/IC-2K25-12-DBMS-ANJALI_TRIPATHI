6b58a0889c37e7c13719717af2ed6df239522e15
Anjali Tripathi <at4289886@gmail.com>
2026-09-04 18:53:05 +0530

    count no. of employees
    Add SQL query to count the number of employees.

---
+++ Lab _Assignment - 2/Question_09.sql
@@ -0,0 +1,10 @@
+ MySQL  localhost:33060+ ssl  dbms_assignment  SQL > SELECT COUNT(employee_id) AS number_of_employees
+                                                  -> FROM employees;
++---------------------+
+| number_of_employees |
++---------------------+
+|                  10 |
++---------------------+
+1 row in set (0.0053 sec)
+ MySQL  localhost:33060+ ssl  dbms_assignment  SQL >
+
