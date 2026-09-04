906501bc8f59b4cab02b983947eb527aa92aee49
Anjali Tripathi <at4289886@gmail.com>
2026-09-04 18:56:24 +0530

    count no of jobs.
    Added SQL queries to display employee data and job count.

---
+++ Lab _Assignment - 2/Question_10.sql
@@ -0,0 +1,41 @@
+ MySQL  localhost:33060+ ssl  SQL > use dbms_assignment;
+Default schema set to `dbms_assignment`.
+Fetching global names, object names from `dbms_assignment` for auto-completion... Press ^C to stop.
+ MySQL  localhost:33060+ ssl  dbms_assignment  SQL > show tables;
++---------------------------+
+| Tables_in_dbms_assignment |
++---------------------------+
+| countries_null            |
+| department                |
+| dup_countries             |
+| employee                  |
+| employees                 |
+| job_histry                |
+| jobs                      |
++---------------------------+
+7 rows in set (0.0032 sec)
+ MySQL  localhost:33060+ ssl  dbms_assignment  SQL > select*from employees;
++-------------+------------+-----------+------------------+--------------+------------+--------+----------+------------+------------+---------------+
+| employee_id | first_name | last_name | email            | phone_number | hire_date  | job_id | salary   | commission | manager_id | department_id |
++-------------+------------+-----------+------------------+--------------+------------+--------+----------+------------+------------+---------------+
+|           1 | Rahul      | Sharma    | rahul@gmail.com  | 9876543210   | 2023-01-10 |      1 | 30000.00 |    2000.00 |        101 |            10 |
+|           2 | Priya      | Verma     | priya@gmail.com  | 9876543211   | 2023-02-15 |      2 | 35000.00 |    2500.00 |        102 |            20 |
+|           3 | Aman       | Patel     | aman@gmail.com   | 9876543212   | 2023-03-20 |      3 | 28000.00 |    1500.00 |        101 |            10 |
+|           4 | Neha       | Singh     | neha@gmail.com   | 9876543213   | 2023-04-05 |      1 | 40000.00 |    3000.00 |        103 |            30 |
+|           5 | Riya       | Gupta     | riya@gmail.com   | 9876543214   | 2023-05-12 |      2 | 32000.00 |    2200.00 |        102 |            20 |
+|           6 | Karan      | Mehta     | karan@gmail.com  | 9876543215   | 2023-06-18 |      3 | 45000.00 |    3500.00 |        103 |            30 |
+|           7 | Ankit      | Jain      | ankit@gmail.com  | 9876543216   | 2023-07-22 |      1 | 25000.00 |    1000.00 |        101 |            10 |
+|           8 | Pooja      | Yadav     | pooja@gmail.com  | 9876543217   | 2023-08-10 |      2 | 38000.00 |    2800.00 |        102 |            20 |
+|           9 | Vikas      | Shah      | vikas@gmail.com  | 9876543218   | 2023-09-14 |      3 | 50000.00 |    4000.00 |        103 |            30 |
+|          10 | Simran     | Khan      | simran@gmail.com | 9876543219   | 2023-10-25 |      1 | 42000.00 |    3200.00 |        101 |            10 |
++-------------+------------+-----------+------------------+--------------+------------+--------+----------+------------+------------+---------------+
+10 rows in set (0.0014 sec)
+  MySQL  localhost:33060+ ssl  dbms_assignment  SQL > SELECT COUNT(job_id) AS number_of_jobs
+                                                  -> FROM employees;
++----------------+
+| number_of_jobs |
++----------------+
+|             10 |
++----------------+
+1 row in set (0.0019 sec)
+ MySQL  localhost:33060+ ssl  dbms_assignment  SQL >
